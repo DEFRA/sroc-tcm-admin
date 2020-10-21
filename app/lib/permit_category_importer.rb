4c69175d5cf1537751cfafa616a4f09e7bc28ab8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-21 15:51:00 +0100

    Fix Naming/VariableName
    ```
    bundle exec rubocop --format offenses
    bundle exec rubocop --only Naming/VariableName
    ```
    
    Manual fix

--- app/lib/permit_category_importer.rb
+++ app/lib/permit_category_importer.rb
@@ -7,13 +7,13 @@ class PermitCategoryImporter
     Thread.current[:current_user] = User.system_account
 
     n = regime.permit_categories.count
-    destroy_BOM = true
+    destroy_bom = true
     CSV.foreach(filename, headers: false) do |row|
       code = row[0]
       desc = row[1]
-      if destroy_BOM
+      if destroy_bom
         code = TcmUtils.strip_bom(code)
-        destroy_BOM = false
+        destroy_bom = false
       end
       n += 1
       regime.permit_categories.find_or_create_by!(code: code) do |cat|
