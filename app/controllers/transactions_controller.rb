c65f9994fb10db87cbe9e911443df168859fce02
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-21 23:25:56 +0100

    Fix again Layout/SpaceInsideHashLiteralBraces
    ```
    bundle exec rubocop --format offenses
    bundle exec rubocop --only Layout/SpaceInsideHashLiteralBraces
    bundle exec rubocop --auto-correct --only Layout/SpaceInsideHashLiteralBraces
    ```

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -72,7 +72,7 @@ class TransactionsController < ApplicationController
         end
         format.json do
           render json: { transaction: presenter.new(@transaction, current_user),
-                         message: "Transaction updated"},
+                         message: "Transaction updated" },
                  status: :ok,
                  location: path
         end
