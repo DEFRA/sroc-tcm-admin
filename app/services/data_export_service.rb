95a9734775778bab548461737716ac4fc3385512
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-05-05 11:51:29 +0100

    Remove `.with_connection` block from service
    We've not needed to use this in other jobs and testing confirms we don't need it in the service. Removing it as unnecessary.
    
    Note - There are also some initial puts() debug calls. We'll firm these up as we iterate the service.

--- app/services/data_export_service.rb
+++ app/services/data_export_service.rb
@@ -7,20 +7,22 @@ class DataExportService < ServiceObject
 
   def call
     @result = false
-    ActiveRecord::Base.connection_pool.with_connection do
-      Regime.all.each do |regime|
-        result = ExportTransactionData.call(regime: regime, batch_size: 1000)
-        if result.failed?
-          TcmLogger.error("Failed to export transactions for #{regime.name}")
-        else
-          # store file
-          result = PutDataExportFile.call(filename: result.filename)
-          TcmLogger.error("Failed to store export data file for #{regime.name}") if result.failed?
-        end
+    puts("Started data export")
+    Regime.all.each do |regime|
+      puts("Checking regime #{regime.name}")
+      result = ExportTransactionData.call(regime: regime, batch_size: 1000)
+      if result.failed?
+        TcmLogger.error("Failed to export transactions for #{regime.name}")
+      else
+        # store file
+        result = PutDataExportFile.call(filename: result.filename)
+        TcmLogger.error("Failed to store export data file for #{regime.name}") if result.failed?
       end
     end
     @result = true
   rescue StandardError => e
     TcmLogger.notify(e)
+  ensure
+    puts("Finished data export")
   end
 end
