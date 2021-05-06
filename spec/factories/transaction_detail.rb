be6c83f511511abcde3a745964d88770c3ac1a3b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-05-06 09:06:32 +0100

    Rubocop!

--- spec/factories/transaction_detail.rb
+++ spec/factories/transaction_detail.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
     transaction_reference { rand(36**8).to_s(36).upcase }
     currency_code { "GBP" }
     header_attr_1 { 2.months.ago.strftime("%d-%^b-%Y") }
-    line_amount { 23747 }
+    line_amount { 23_747 }
     line_description { "Consent No - TONY/1234/1/1" }
     line_area_code { "3" }
     line_income_stream_code { "C" }
@@ -29,13 +29,13 @@ FactoryBot.define do
     reference_2 { "1" }
     reference_3 { "1" }
     unit_of_measure { "Each" }
-    unit_of_measure_price { 23747 }
+    unit_of_measure_price { 23_747 }
     period_start { "01-Apr-2017" }
     period_end { "10-Aug-2017" }
     original_filename { "CFDBI00123" }
     original_file_date { 2.weeks.ago.to_date }
     tcm_financial_year { "1718" }
-    region { 'A' }
+    region { "A" }
 
     association :transaction_header, factory: :transaction_header
   end
