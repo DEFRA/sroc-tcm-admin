82d3cc1b7a8040676a037fb1588686ceecec4953
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-05-07 13:26:33 +0100

    Return self from call()
    Overlooked that the pattern for services is that they return `self` as the result of `call()`.
    
    We need this to assert whether the service is reporting `success?()` or `failed?()` when its done.

--- app/services/data_export_service.rb
+++ app/services/data_export_service.rb
@@ -7,22 +7,25 @@ class DataExportService < ServiceObject
 
   def call
     @result = false
-    puts("Started data export")
-    Regime.all.each do |regime|
-      puts("Checking regime #{regime.name}")
-      result = ExportTransactionDataService.call(regime: regime, batch_size: 1000)
-      if result.failed?
-        TcmLogger.error("Failed to export transactions for #{regime.name}")
-      else
-        # store file
-        result = PutDataExportFileService.call(filename: result.filename)
-        TcmLogger.error("Failed to store export data file for #{regime.name}") if result.failed?
+    begin
+      puts("Started data export")
+      Regime.all.each do |regime|
+        puts("Checking regime #{regime.name}")
+        result = ExportTransactionDataService.call(regime: regime, batch_size: 1000)
+        if result.failed?
+          TcmLogger.error("Failed to export transactions for #{regime.name}")
+        else
+          # store file
+          result = PutDataExportFileService.call(filename: result.filename)
+          TcmLogger.error("Failed to store export data file for #{regime.name}") if result.failed?
+        end
       end
+      @result = true
+    rescue StandardError => e
+      TcmLogger.notify(e)
+    ensure
+      puts("Finished data export")
     end
-    @result = true
-  rescue StandardError => e
-    TcmLogger.notify(e)
-  ensure
-    puts("Finished data export")
+    self
   end
 end
