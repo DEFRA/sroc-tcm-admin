e27e6f91e42eae80a257ffa70237ce8eb974d38b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-06-13 10:49:45 +0100

    Remove resque and redis code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,10 +54,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "sroc-tcm-admin_#{Rails.env}"
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
