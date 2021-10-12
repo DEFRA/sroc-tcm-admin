26f2478ed79c5a2b662446cbee5987bc9929cc45
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-10-12 11:28:19 +0100

    Remove local file store (#472)
    The TCM currently has the functionality to replicate reading and writing things to AWS by using the local file system instead.
    
    Our issues with this are
    
    - if you are new to the project or haven't been using it for a while it can be easy to forget this is the case
    - it adds additional complexity and code (which is untested) for something intended to make working locally and writing tests easier
    
    The preference of the existing tech team is
    
    - the app behaves the same across all environments
    - to stub or mock an external service rather than try and replicate it in tests
    - to have less complexity and code to maintain
    
    So, this change removes using the local file system as a stand-in for AWS S3 and all the logic that supports this feature.
    
    ** Notes
    
    - Add new S3 list response template
    
    When you send a `GET` list request to an AWS S3 bucket you get an xml response. There is a command built into the AWS S3 SDK which can interpret that response which AWSFileStore depends on.
    
    As we are aiming to stub requests to S3 instead of depending on the local file system we needed a way we could mimic that response from AWS. The S3 list template is our way of doing it.
    
    - Update all tests that depended on local file store
    
    With this change all the tests that interacted with the local file system as a means of setting up a test or verifying behaviour have been updated to depend on stubbing the AWS requests and responses.
    
    Ideally, we'd just stub/mock the services that the service under test calls. But we're aiming for as much test coverage as possible. So, by stubbing the requests, which is the furthest away from these services under test, more of the TCM's code will be hit when the test runs.
    
    As we improve our unit test coverage we can hopefully revisit this and move the stubbing/mocking closer to the code under test.
    
    - Move TransactionFileExporter
    
    With the removal of the local file store we have found that some of the minitest tests are now failing. Rather than update minitest to work with webmock and stub all its S3 requests we take this opportunity to migrate those affected to RSpec.
    
    Adding webmock to minitest also breaks all the integration tests making sorting out minitest a much bigger task. Migrating the tests is what we want to do and in comparison is probably of a similar effort.
    
    So, we want to add an RSpec test. The next thing that hit us is that the class is not in fact a service. It probably should be, but currently, it's just a PORO and doesn't implement the TCM service object pattern.
    
    So, to make this clearer we moved the class to the `app/lib` folder.
    
    - Convert TransactionFileExporterTest to rspec
    
    After making the change we confirmed the unit test coverage for TransactionFileExporterTest is exactly the same. But the new test covers 15% of the code compared to 11% so we at least get a bump there.
    
    - Add comment about pluck to filters
    
    We had to research pluck() as part of migrating the tests from minitest to RSpec. Rather than forget it and have to research it again later we added this note for those not familiar with the function.

--- spec/services/put_data_export_file_service_spec.rb
+++ spec/services/put_data_export_file_service_spec.rb
@@ -3,30 +3,29 @@
 require "rails_helper"
 
 RSpec.describe PutDataExportFileService do
-  let(:archive_path) { Rails.root.join(LocalFileStore.new("archive_bucket").base_path, "csv") }
-  let(:fixture_path) { Rails.root.join("spec", "fixtures", "export_files") }
   let(:service) { PutDataExportFileService }
-
-  after(:each) do
-    # Clean up - ensure any files we create irrespective of whether the test is successful or not are deleted
-    Helpers::FileHelpers.clean_up(export_file, archive_path)
-  end
+  let(:test_export_full_file_name) { Helpers::FileHelpers.fixture_path(export_file_name, "export_files") }
+  let(:s3_archives_regex) { Helpers::S3Helpers.archives_regex("csv", export_file_name) }
 
   describe "#call" do
+    let(:s3_status) { 200 }
+
+    before(:each) do
+      stub_request(:put, s3_archives_regex).to_return(status: s3_status)
+    end
+
     context "when the file exists" do
-      let(:export_file) { "cfd_transactions.csv" }
+      let(:export_file_name) { "cfd_transactions.csv" }
 
       it "copys the file to the archive csv folder" do
-        service.call(filename: File.join(fixture_path, export_file))
+        service.call(filename: test_export_full_file_name)
 
-        # We check both the source and the destination folders to confirm the file was just copied, and not copied then
-        # deleted
-        expect(File.exist?(File.join(archive_path, export_file))).to be(true)
-        expect(File.exist?(File.join(fixture_path, export_file))).to be(true)
+        # Confirm a request is made to upload the file to the S3 archive bucket as well
+        expect(WebMock).to have_requested(:put, s3_archives_regex).once
       end
 
-      it "marks the export as failed" do
-        result = service.call(filename: File.join(fixture_path, export_file))
+      it "marks the export as succeeded" do
+        result = service.call(filename: test_export_full_file_name)
 
         expect(result.success?).to be(true)
         expect(result.failed?).to be(false)
@@ -34,10 +33,10 @@ RSpec.describe PutDataExportFileService do
     end
 
     context "when the file does not exist" do
-      let(:export_file) { "foo.csv" }
+      let(:export_file_name) { "foo.csv" }
 
       it "marks the export as failed" do
-        result = service.call(filename: File.join(fixture_path, export_file))
+        result = service.call(filename: test_export_full_file_name)
 
         expect(result.success?).to be(false)
         expect(result.failed?).to be(true)
@@ -45,14 +44,11 @@ RSpec.describe PutDataExportFileService do
     end
 
     context "when an error is thrown" do
-      let(:export_file) { "cfd_transactions.csv" }
-
-      before(:each) do
-        allow_any_instance_of(FileStorage).to receive(:archive_file_store).and_raise("boom")
-      end
+      let(:export_file_name) { "cfd_transactions.csv" }
+      let(:s3_status) { 403 }
 
       it "marks the export as failed" do
-        result = service.call(filename: File.join(fixture_path, export_file))
+        result = service.call(filename: test_export_full_file_name)
 
         expect(result.success?).to be(false)
         expect(result.failed?).to be(true)
--- spec/support/helpers/file_helpers.rb
+++ spec/support/helpers/file_helpers.rb
@@ -2,11 +2,25 @@
 
 module Helpers
   module FileHelpers
+    def self.fixture_path(fixture_filename, fixture_path = "")
+      File.join(fixtures_path(fixture_path), fixture_filename)
+    end
+
+    def self.fixtures_path(path = "")
+      Rails.root.join("spec", "fixtures", path)
+    end
+
     def self.clean_up(filename, *folders)
       return unless filename
 
       full_path = File.join(*folders, filename)
       File.delete(full_path) if File.exist?(full_path)
+    end
+
+    def self.fixture_content(fixture_filename, fixture_path = "")
+      fixture = File.open(fixture_path(fixture_filename, fixture_path))
+
+      fixture.read
     end
   end
 end
