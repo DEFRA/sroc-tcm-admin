0255088a9b1eb4029932d8812f1f199962d40dea
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-10-21 12:19:36 +0100

    Remove react and webpack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # recompile webpack files as we make changes
-  config.x.webpacker[:dev_server_host]
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
