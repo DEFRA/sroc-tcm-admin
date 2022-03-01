fefc84f740d877fc10d96b43235339082328ac4c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-03-01 17:27:44 +0000

    Add unit tests
    As we're adding the logic directly into the TCM we need unit tests to cover it. Again, these are pretty much taken from Defra Ruby Email (DFE). Key differences are
    
    - in DFE we have a configuration class that handles whether the endpoint is enabled. We mock it rather than stub `ENV`
    - DFE is intended to handle all types of emails. We know we never send anything with an attachement. So, we don't have all the unit tests DFE does
    - DFE has a test mailer to support the unit tests and to check whether an app is setup for emailing. We just have the one mailer in TCM which isn't setup to support testing each email type. So, we had a test mailer into spec/support

---
+++ spec/lib/last_email_cache_spec.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe LastEmailCache do
+  subject(:instance) { described_class.instance }
+
+  before(:each) { instance.reset }
+
+  let(:recipient) { "test@example.com" }
+  let(:add_attachment) { false }
+  let(:expected_keys) { %w[date from to bcc cc reply_to subject body attachments] }
+
+  describe "#last_email_json" do
+
+    context "when the no emails have been sent" do
+      let(:expected_keys) { %w[error] }
+
+      it "returns a JSON string" do
+        result = instance.last_email_json
+
+        expect(result).to be_a(String)
+        expect { JSON.parse(result) }.to_not raise_error
+      end
+
+      it "responds with an error message" do
+        result = JSON.parse(instance.last_email_json)
+
+        expect(result.keys).to match_array(expected_keys)
+      end
+    end
+
+    context "when a basic email is sent" do
+      context "and it is formatted as plain text" do
+        before(:each) { TestMailer.text_email(recipient).deliver_now }
+
+        it "returns a JSON string" do
+          result = instance.last_email_json
+          puts "result was #{result}"
+
+          expect(result).to be_a(String)
+          expect { JSON.parse(result) }.to_not raise_error
+        end
+
+        it "contains the attributes of the email" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"].keys).to match_array(expected_keys)
+        end
+
+        it "extracts the plain text body content" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"]["body"]).to start_with("This is the text version of an email")
+        end
+      end
+
+      context "and it is formatted as html" do
+        before(:each) { TestMailer.html_email(recipient).deliver_now }
+
+        it "returns a JSON string" do
+          result = instance.last_email_json
+
+          expect(result).to be_a(String)
+          expect { JSON.parse(result) }.to_not raise_error
+        end
+
+        it "contains the attributes of the email" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"].keys).to match_array(expected_keys)
+        end
+
+        it "extracts the html body content" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"]["body"]).to start_with("<h1>This is the html version of an email</h1>")
+        end
+      end
+    end
+
+    # Multi-part essentially means the email contains more than 2 elements. An
+    # element can be a HTML version, and plain text version, and an
+    # attachment. If it contains at least 2 of these it will be sent as a
+    # multipart email
+    context "when a multi-part email is sent" do
+      context "and it contains both a html and text version" do
+        before(:each) { TestMailer.multipart_email(recipient).deliver_now }
+
+        it "returns a JSON string" do
+          result = instance.last_email_json
+
+          expect(result).to be_a(String)
+          expect { JSON.parse(result) }.to_not raise_error
+        end
+
+        it "contains the attributes of the email" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"].keys).to match_array(expected_keys)
+        end
+
+        it "extracts the plain text body content" do
+          result = JSON.parse(instance.last_email_json)
+
+          expect(result["last_email"]["body"]).to start_with("This is the text version of an email")
+        end
+      end
+    end
+
+    context "when multiple emails have been sent" do
+      before(:each) do
+        TestMailer.text_email(recipient).deliver_now
+        TestMailer.text_email(last_recipient).deliver_now
+      end
+
+      let(:last_recipient) { "joe.bloggs@example.com" }
+
+      it "returns a JSON string" do
+        result = instance.last_email_json
+
+        expect(result).to be_a(String)
+        expect { JSON.parse(result) }.to_not raise_error
+      end
+
+      it "contains the attributes of the email" do
+        result = JSON.parse(instance.last_email_json)
+
+        expect(result["last_email"].keys).to match_array(expected_keys)
+      end
+
+      it "extracts the plain text body content" do
+        result = JSON.parse(instance.last_email_json)
+
+        expect(result["last_email"]["body"]).to start_with("This is the text version of an email")
+      end
+
+      it "contains the details of the last email sent" do
+        result = JSON.parse(instance.last_email_json)
+
+        expect(result["last_email"]["to"]).to eq([last_recipient])
+      end
+    end
+  end
+end
---
+++ spec/requests/last_email_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "LastEmail", type: :request do
+  describe "/last-email" do
+    before(:each) {  stub_const("ENV", ENV.to_hash.merge("ENABLE_LAST_EMAIL" => enable_last_email)) }
+
+    context "when the env var ENABLE_LAST_EMAIL is 'true'" do
+      let(:enable_last_email) { "true" }
+
+      before(:each) { TestMailer.text_email("test@example.com").deliver_now }
+
+      it "returns the JSON value of the LastEmailCache" do
+        get last_email_path
+
+        expect(response.body).to eq(LastEmailCache.instance.last_email_json)
+        expect(LastEmailCache.instance.last_email_json).to include("test@example.com")
+      end
+    end
+
+    context "when the env var ENABLE_LAST_EMAIL is 'false'" do
+      let(:enable_last_email) { "false" }
+
+      it "cannot load the page" do
+        expect { get last_email_path }.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
+end
