ba2ca662c5a6d5b95233c19991396e7ca815e6f7
sweavers-defra <108677100+sweavers-defra@users.noreply.github.com>
2023-02-20 15:26:39 +0000

    Parameterise the AWS bucket region (#632)
    The current infrastructure runs in the `eu-west-1` region. The new ECS infrastructure is run in `eu-west-2` region. 
    
    To enable concurrent functionality, the region parameter must be able to be changed as required. The solution is via a new environment variable.

--- spec/support/helpers/s3_helpers.rb
+++ spec/support/helpers/s3_helpers.rb
@@ -3,23 +3,23 @@
 module Helpers
   module S3Helpers
     def self.any_request
-      %r{https://.*\.s3\.eu-west-1\.amazonaws\.com/.*}
+      %r{https://.*\.s3\.#{ENV["BUCKET_AWS_REGION"]}\.amazonaws\.com/.*}
     end
 
     def self.any_bucket_regex(path = "", file_name = "")
-      %r{https://.*\.s3\.eu-west-1\.amazonaws\.com/#{path}/#{file_name}}
+      %r{https://.*\.s3\.#{ENV["BUCKET_AWS_REGION"]}\.amazonaws\.com/#{path}/#{file_name}}
     end
 
     def self.list_uploads_regex(path = "")
-      %r{https://.*\.s3\.eu-west-1\.amazonaws\.com/\?list-type=2&prefix=#{path}}
+      %r{https://.*\.s3\.#{ENV["BUCKET_AWS_REGION"]}\.amazonaws\.com/\?list-type=2&prefix=#{path}}
     end
 
     def self.uploads_regex(path = "", file_name = "")
-      %r{https://#{ENV["FILE_UPLOAD_BUCKET"]}\.s3\.eu-west-1\.amazonaws\.com/#{path}/#{file_name}}
+      %r{https://#{ENV["FILE_UPLOAD_BUCKET"]}\.s3\.#{ENV["BUCKET_AWS_REGION"]}\.amazonaws\.com/#{path}/#{file_name}}
     end
 
     def self.archives_regex(path = "", file_name = "")
-      %r{https://#{ENV["ARCHIVE_BUCKET"]}\.s3\.eu-west-1\.amazonaws\.com/#{path}/#{file_name}}
+      %r{https://#{ENV["ARCHIVE_BUCKET"]}\.s3\.#{ENV["BUCKET_AWS_REGION"]}\.amazonaws\.com/#{path}/#{file_name}}
     end
 
     def self.list_response(path = "", file_name = "")
